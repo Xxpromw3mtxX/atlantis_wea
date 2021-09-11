c3babbde955be6fdca58d2ed0dfe68a510269fdb
Francesco Metta <fra.metta@hotmail.it>
2021-09-11 13:33:56 +0200

    lang files

---
+++ atlantis_wea/locales/en.lua
@@ -0,0 +1,4 @@
+Locales['en'] = {
+    ['no_weapon_compatible'] = 'You have no weapons to mount the component on',
+    ['component_assembled'] = 'The component has been installed'
+}
---
+++ atlantis_wea/locales/it.lua
@@ -0,0 +1,4 @@
+Locales['it'] = {
+    ['no_weapon_compatible'] = 'Non hai armi su cui montare la componente',
+    ['component_assembled'] = 'La componente è stata installata'
+}
